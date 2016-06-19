3ef35d02062075cb6646dd783620901751f8fa31
Richard Japenga <treurmars@gmail.com>
2016-06-19 05:16:27 +0200

    added overlays for replaced elements such as image elements, select elements etc

--- debugmode.js
+++ debugmode.js
@@ -21,6 +21,7 @@
     function initDebugMode() {
       if (sessionStorage.getItem(ref) === 'active') {
         document.body.classList.add(ref);
+        replacedElements();
       }
     };
 
@@ -28,10 +29,42 @@
       if (sessionStorage.getItem(ref) === null) {
         sessionStorage.setItem(ref, 'active');
         document.body.classList.add(ref);
+        replacedElements();
       }
       else {
         sessionStorage.removeItem(ref);
         document.body.classList.remove(ref);
+        removeOverlay();
+      }
+    };
+
+    function replacedElements() {
+      var elements = document.querySelectorAll('img, object, select');
+
+      elements.forEach(addOverlay);
+    };
+
+    function addOverlay(element) {
+      var rec = element.getBoundingClientRect();
+      var overlay = document.createElement('div');
+      console.log(rec);
+      
+      overlay.style.width = rec.width + 'px';
+      overlay.style.height = rec.height + 'px';
+      overlay.style.top = rec.top + 'px';
+      overlay.style.left = rec.left + 'px';
+      overlay.style.backgroundColor = 'red';
+      overlay.style.position = 'absolute';
+      overlay.classList.add('overlay');
+
+      document.body.appendChild(overlay);
+    };
+
+    function removeOverlay() {
+      var overlays = document.querySelectorAll('.overlay');
+      console.log(overlays);
+      for (var i = 0; i < overlays.length; i++) {
+        overlays[i].parentNode.removeChild(overlays[i]);
       }
     };
   };
